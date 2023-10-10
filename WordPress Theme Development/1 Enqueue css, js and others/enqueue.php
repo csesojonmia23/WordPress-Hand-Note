03254782b837c3464233108f816b76cad6c0807d
Sojon Mia <141360186+csesojonmia23@users.noreply.github.com>
2023-10-10 21:27:26 +0600

    Update enqueue.php

--- WordPress Theme Development/1 Enqueue css, js and others/enqueue.php
+++ WordPress Theme Development/1 Enqueue css, js and others/enqueue.php
@@ -3,23 +3,21 @@
 // Enqueue css and js
 function lazy_css_js_enqueue(){
      // Theme default css
-     wp_enqueue_style( 'lazy-style', get_stylesheet_uri());
-     
+     wp_enqueue_style( 'wptd-style', get_stylesheet_uri());
      // Bootstrap css
-     wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
-     wp_enqueue_style('bootstrap');
+     wp_register_style('bootstrap-css', get_template_directory_uri().'/assets/css/bootstrap.min.css', array(), '5.0.2', 'all');
+     wp_enqueue_style('bootstrap-css');
      // Google fonts
-     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Font1|Font2', array(), null);
+     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap', array(), null);
      // Custom css
-     wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.1.1', 'all');
-     wp_enqueue_style('custom');
-
+     wp_register_style('custom-css', get_template_directory_uri().'/assets/css/custom.css', array(), '1.0.0', 'all');
+     wp_enqueue_style('custom-css');
 
      // Theme default js
      wp_enqueue_script('jquery');
      // Bootstrap js
-     wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array('jquery'), '5.0.2', true);
+     wp_enqueue_script('bootstrap-js', get_template_directory_uri().'/assets/js/bootstrap.bundle.min.js', array('jquery'), '5.0.2', true);
      // Custom JS
-     wp_register_script('custom-script', get_template_directory_uri() . '/js/custom-script.js', array('jquery'), '1.0.0', true);
+     wp_enqueue_script('custom-script', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), '1.0.0', true);
 }
 add_action('wp_enqueue_scripts', 'lazy_css_js_enqueue');

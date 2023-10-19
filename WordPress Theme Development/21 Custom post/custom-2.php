eced9874e42ea5572ed3602c5460bc8daa879822
Sojon Mia <141360186+csesojonmia23@users.noreply.github.com>
2023-10-19 15:01:46 +0600

    Update custom-2.php

--- WordPress Theme Development/21 Custom post/custom-2.php
+++ WordPress Theme Development/21 Custom post/custom-2.php
@@ -1,43 +1,57 @@
 <?php
-
-function create_service_post_type() {
-    $labels = array(
-        'name'               => 'Services',
-        'singular_name'      => 'Service',
-        'add_new'            => 'Add New Service',
-        'add_new_item'       => 'Add New Service',
-        'edit_item'          => 'Edit Service',
-        'new_item'           => 'New Service',
-        'view_item'          => 'View Service',
-        'search_items'       => 'Search Services',
-        'not_found'          => 'No services found',
-        'not_found_in_trash' => 'No services found in Trash',
-        'parent_item_colon'  => 'Parent Service:',
-        'menu_name'          => 'Services',
-    );
-
-    $args = array(
-        'labels'              => $labels,
-        'public'              => true,
-        'has_archive'         => true,
-        'publicly_queryable'  => true,
-        'query_var'           => true,
-        'rewrite'             => array('slug' => 'service'),
-        'capability_type'     => 'post',
-        'hierarchical'        => false,
-        'menu_position'       => 5,
-        'supports'            => array(
-            'title',
-            'editor',
-            'thumbnail',
-            'excerpt',
-            'custom-fields',
-            'page-attributes',
-        ),
-    );
-    register_post_type('testimonial', $args);
-}
-
-add_action('init', 'create_service_post_type');
-
-
+if ( ! function_exists('slider_custom_post') ) {
+     // Register Custom Post Type
+     function slider_custom_post() {
+          $labels = array(
+               'name'                  => _x( 'Sliders', 'Post Type General Name', 'sojonone' ),
+               'singular_name'         => _x( 'Slider', 'Post Type Singular Name', 'sojonone' ),
+               'menu_name'             => __( 'Slider', 'sojonone' ),
+               'name_admin_bar'        => __( 'Slider', 'sojonone' ),
+               'archives'              => __( 'Item Archives', 'sojonone' ),
+               'attributes'            => __( 'Item Attributes', 'sojonone' ),
+               'parent_item_colon'     => __( 'Parent Item:', 'sojonone' ),
+               'all_items'             => __( 'All Items', 'sojonone' ),
+               'add_new_item'          => __( 'Add New Item', 'sojonone' ),
+               'add_new'               => __( 'Add New', 'sojonone' ),
+               'new_item'              => __( 'New Item', 'sojonone' ),
+               'edit_item'             => __( 'Edit Item', 'sojonone' ),
+               'update_item'           => __( 'Update Item', 'sojonone' ),
+               'view_item'             => __( 'View Item', 'sojonone' ),
+               'view_items'            => __( 'View Items', 'sojonone' ),
+               'search_items'          => __( 'Search Item', 'sojonone' ),
+               'not_found'             => __( 'Not found', 'sojonone' ),
+               'not_found_in_trash'    => __( 'Not found in Trash', 'sojonone' ),
+               'featured_image'        => __( 'Featured Image', 'sojonone' ),
+               'set_featured_image'    => __( 'Set featured image', 'sojonone' ),
+               'remove_featured_image' => __( 'Remove featured image', 'sojonone' ),
+               'use_featured_image'    => __( 'Use as featured image', 'sojonone' ),
+               'insert_into_item'      => __( 'Insert into item', 'sojonone' ),
+               'uploaded_to_this_item' => __( 'Uploaded to this item', 'sojonone' ),
+               'items_list'            => __( 'Items list', 'sojonone' ),
+               'items_list_navigation' => __( 'Items list navigation', 'sojonone' ),
+               'filter_items_list'     => __( 'Filter items list', 'sojonone' ),
+          );
+          $args = array(
+               'menu_icon'             => 'dashicons-slides',
+               'label'                 => __( 'Slider', 'sojonone' ),
+               'description'           => __( 'This is slider post. You can create slider from here.', 'sojonone' ),
+               'labels'                => $labels,
+               'supports'              => array( 'title', 'editor', 'thumbnail' ),
+               'hierarchical'          => false,
+               'public'                => true,
+               'show_ui'               => true,
+               'show_in_menu'          => true,
+               'menu_position'         => 5,
+               'show_in_admin_bar'     => true,
+               'show_in_nav_menus'     => true,
+               'can_export'            => true,
+               'has_archive'           => true,
+               'exclude_from_search'   => false,
+               'publicly_queryable'    => true,
+               'capability_type'       => 'page',
+          );
+          register_post_type( 'slider', $args );
+     }
+     add_action( 'init', 'slider_custom_post', 0 );
+     
+     }
